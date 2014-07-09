3d24ecaa4e8f2ed3a7f0241d7d7436cab6d14627
Justin Clark-Casey <justincc@justincc.org>
2014-07-09 22:45:26 +0100

    Update sample web page now that Events table has been renamed to UserRegionEvents

--- index.php
+++ index.php
@@ -6,19 +6,19 @@
 $conn = mysql_connect("localhost", "root", "passw0rd");
 mysql_select_db("eventrecorder", $conn);
 
-$res = mysql_query("select * from Events", $conn);
+$res = mysql_query("select * from UserRegionEvents", $conn);
 
 # print_r($count);
 echo "<h1>Summary</h1>\n";
 echo "Got " . mysql_num_rows($res) . " events.\n";
 
-$res = mysql_query("select UserName, date(DateTime) as Date from Events where Type=\"login\" group by date(DateTime);");
+$res = mysql_query("select UserName, date(DateTime) as Date from UserRegionEvents where Type=\"login\" group by date(DateTime);");
 
 echo "<h1>Logins</h1>\n";
 
 while ($row = mysql_fetch_assoc($res))
 {
-  echo "User " . $row['UserName'] . " on " . $row['Date'] . "\n";
+  echo "User " . $row['UserName'] . " on " . $row['Date'] . "</br>\n";
 }
 
 ?>
